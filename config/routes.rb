d539f862f859bb57eaf9935a47070b48e8d597c5
GG Gonzalez <gg.gonzalez@ymail.com>
2018-01-20 18:37:01 -0500

    backend auth done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
   root 'welcome#index'
   resources :categories, only: [:index, :show] do
     resources :questions, only: [:index, :show]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-Category.create([
-  {name: "Pop Culture Factoids and Questions"},
-  {name: "Sports"},
-  {name: "Random Facts"},
-  {name: "Minimally-Invasive Personal Questions"},
-  {name: "Current Events"}
-])
-
-
-Question.create([
-  {
-    content: "",
-    category_id: 1
-  }
-
-  ])
+# Category.create([
+#   {name: "Pop Culture Factoids and Questions"},
+#   {name: "Sports"},
+#   {name: "Random Facts"},
+#   {name: "Minimally-Invasive Personal Questions"},
+#   {name: "Current Events"}
+# ])
+#
+#
+# Question.create([
+#   {
+#     content: "",
+#     category_id: 1
+#   }
+#
+#   ])
