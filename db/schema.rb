d24a53ba5bd670ee945c1a3adef173a84d7f6705
GG Gonzalez <gg.gonzalez@ymail.com>
2018-01-27 14:51:43 -0500

    got faves heart red

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_01_26_215606) do
+ActiveRecord::Schema.define(version: 2018_01_27_164747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_01_26_215606) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "favorites", "questions"
